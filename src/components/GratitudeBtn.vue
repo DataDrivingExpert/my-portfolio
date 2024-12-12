82ce007a0d89922cee39295e7f2fceb04cc2a938
DataDrivingExpert <herrera.cuevasO.E@gmail.com>
2024-12-12 11:54:02 -0300

    Sección de agradecimientos Se incorpora la sección de agradecimientos, principalmente para referenciar a los autores del contenido multimedia presentado en el sitio web. Aún en construcción.

---
+++ src/components/GratitudeBtn.vue
@@ -0,0 +1,17 @@
+<template>
+    <div
+      class="flex flex-row rounded-full bg-rose-700/50 text-white px-5 py-2 w-fit justify-evenly gap-x-3 cursor-pointer hover:bg-rose-700"
+    >
+      <VideoCameraIcon class="h-6 w-6 text-white" />
+
+    </div>
+  </template>
+  
+  <script setup>
+  import { VideoCameraIcon } from "@heroicons/vue/24/outline";
+  
+  defineProps({
+    name: String,
+  });
+  </script>
+  
